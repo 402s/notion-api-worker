f03cf861d9b33890508186e83e293afd44fab3b6
shaunregenbaum <shaunregenbaum@gmail.com>
2022-10-12 17:53:56 -0400

    no secrets

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Router, Route, Request } from 'itty-router'
 import { Client } from '@notionhq/client';
 
-const notion = new Client({ auth: NOTION_API_KEY });
 
 interface IMethods {
   get: Route
@@ -13,23 +12,43 @@ const router = Router<Request, IMethods>()
 
 router.get('/', (request: Request) => { return new Response('Hello world') })
 
+// get the code from callback url
+router.get('/callback', (request: Request) => {
+    const url = new URL(request.url)
+    const code = url.searchParams.get('code')
+    return new Response(code)
+})
+
+// using code get an oauth token by making a post request to https://api.notion.com/v1/oauth/token
+router.post('/token', (request: Request) => {
+    const url = new URL(request.url)
+    const code = url.searchParams.get('code')
+});
+    
+router.post('/callback', async (request: Request) => {
+    const url = new URL(request.url)
+    const code = url.searchParams.get('code')
+});
+
 router.get('/login', () => { return Response.redirect('https://api.notion.com/v1/oauth/authorize?client_id=821734af-9bf0-47d8-bfbd-0c2f14efb15f&response_type=code')
- })
+ });
 
 // Take search request and seperate id and pass to query
-router.get('/search', async(request: Request) => {
+router.get('/search', async() => {
+    const notion = new Client({ auth: "" });
     const response = await notion.search({
-    query: "",
+    query: "shaun",
     sort: {
         direction: 'ascending',
         timestamp: 'last_edited_time',
     },
     });
     console.log(response);
-    return new Response(JSON.stringify(response));
+    return new Response(JSON.stringify(response.results));
 })
 
 router.get('/page/:id', async(request: Request) => {
+    const notion = new Client({ auth: "" });
     const { id } = request.params
     const pageId = id;
     const response = await notion.pages.retrieve({ page_id: pageId });
