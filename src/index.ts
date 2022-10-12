5ccb06e6bdd645d0382648d6c33e70b0f85d063d
shaunregenbaum <shaunregenbaum@gmail.com>
2022-10-12 16:44:55 -0400

    removed stuff

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { Router, Route, Request } from 'itty-router'
+const { Client } = require('@notionhq/client');
+
+const notion = new Client({ auth: NOTION_API_KEY });
 
 interface IMethods {
   get: Route
@@ -8,8 +11,19 @@ interface IMethods {
 
 const router = Router<Request, IMethods>()
 
+function search(query){(async () => {
+    const response = await notion.search({
+      query: query,
+      sort: {
+        direction: 'ascending',
+        timestamp: 'last_edited_time',
+      },
+    });
+    return(response);
+  })()};
+
 router.get('/', (request: Request) => { return new Response('Hello world') })
-router.get('/search', (request: Request) => { return new Response('Search') })
+router.get('/search', (request: Request) => { return search("") })
 
 
 addEventListener('fetch', (event:FetchEvent) => {
