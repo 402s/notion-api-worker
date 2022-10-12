91dbf24b1095e23f75497692cb836c2733b718d3
shaunregenbaum <shaunregenbaum@gmail.com>
2022-10-12 17:13:21 -0400

    getting closer

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Router, Route, Request } from 'itty-router'
-const { Client } = require('@notionhq/client');
+import { Client } from '@notionhq/client';
 
 const notion = new Client({ auth: NOTION_API_KEY });
 
@@ -11,19 +11,30 @@ interface IMethods {
 
 const router = Router<Request, IMethods>()
 
-function search(query){(async () => {
+router.get('/', (request: Request) => { return new Response('Hello world') })
+
+router.get('/login', () => { return Response.redirect('https://api.notion.com/v1/oauth/authorize?client_id=821734af-9bf0-47d8-bfbd-0c2f14efb15f&response_type=code')
+ })
+
+// Take search request and seperate id and pass to query
+router.get('/search', async(request: Request) => {
     const response = await notion.search({
-      query: query,
-      sort: {
+    query: "",
+    sort: {
         direction: 'ascending',
         timestamp: 'last_edited_time',
-      },
+    },
     });
-    return(response);
-  })()};
+    console.log(response);
+    return new Response(JSON.stringify(response));
+})
 
-router.get('/', (request: Request) => { return new Response('Hello world') })
-router.get('/search', (request: Request) => { return search("") })
+router.get('/page/:id', async(request: Request) => {
+    const { id } = request.params
+    const pageId = id;
+    const response = await notion.pages.retrieve({ page_id: pageId });
+    return new Response(JSON.stringify(response));
+});
 
 
 addEventListener('fetch', (event:FetchEvent) => {
