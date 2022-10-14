efc4e69f7a63840db31d6b54bce4d66bb1f03440
shaunregenbaum <shaunregenbaum@gmail.com>
2022-10-14 13:30:32 -0400

    the worker is finall fricking workinggit add . git push

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { Router, Route, Request } from 'itty-router'
-import { Client } from '@notionhq/client';
+import { Router, Route, Request } from "itty-router"
+import { Client } from "@notionhq/client";
 
 
 interface IMethods {
@@ -10,52 +10,66 @@ interface IMethods {
 
 const router = Router<Request, IMethods>()
 
-router.get('/', (request: Request) => { return new Response('Hello world') })
-
-// get the code from callback url
-router.get('/callback', (request: Request) => {
-    const url = new URL(request.url)
-    const code = url.searchParams.get('code')
-    return new Response(code)
+router.get("/", () => { 
+    return new Response(`Hello Wooooooorld!`)
 })
-
-// using code get an oauth token by making a post request to https://api.notion.com/v1/oauth/token
-router.post('/token', (request: Request) => {
-    const url = new URL(request.url)
-    const code = url.searchParams.get('code')
-});
     
-router.post('/callback', async (request: Request) => {
-    const url = new URL(request.url)
-    const code = url.searchParams.get('code')
-    return new Response(code);
+
+router.get("/callback", async ({query}) => {
+    const response = await fetch("https://api.notion.com/v1/oauth/token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+             //@ts-ignore as vars are in wrangler env
+            "Authorization": "Basic " + JSON.stringify(btoa(Client_ID + ":" + Client_Secret))
+        },
+        redirect: "follow",
+        body: JSON.stringify({
+            "grant_type": "authorization_code",
+            "code": query.code,
+        })
+    }).then((response) => {  
+        console.log("Success");
+        return response.json();
+    }).then((data)=> {
+        console.log(data);
+        console.log("Success2");
+        return(data)
+    }).catch((error) => {
+        // console.log(error);
+        console.log("Error");
+        return JSON.stringify(error);
+    });
+    return new Response(JSON.stringify(response.error));
 });
 
-router.post('/dev/callback', async (request: Request) => {
+router.post("/dev/callback", async (request: Request) => {
     const url = new URL(request.url)
-    const code = url.searchParams.get('code')
+    const code = url.searchParams.get("code")
     console.log(code)
-    return Response.redirect('http://localhost:3000/callback'+ '?code=' + code, 301);
+
+    return Response.redirect("http://localhost:8787/callback?code=" + code, 302);
 });
 
-router.get('/login', () => { return Response.redirect('https://api.notion.com/v1/oauth/authorize?client_id=b29cc2b5-0184-4adb-8b19-60aaf0e859dd&response_type=code', 301)
+router.get("/login", () => { 
+    return Response.redirect("https://api.notion.com/v1/oauth/authorize?client_id=b29cc2b5-0184-4adb-8b19-60aaf0e859dd&response_type=code", 301)
  });
 
 // Take search request and seperate id and pass to query
-router.get('/search', async() => {
-    const notion = new Client({ auth: "" });
+router.get("/search", async() => {
+    const notion = new Client({ auth: "secret_ykuAumQjK7GxXmUVaWLWDXzAfwUhHXDhi1nB8cHpA7G" });
     const response = await notion.search({
-    query: "shaun",
+    query: "",
     sort: {
-        direction: 'ascending',
-        timestamp: 'last_edited_time',
+        direction: "ascending",
+        timestamp: "last_edited_time",
     },
     });
     console.log(response);
     return new Response(JSON.stringify(response.results));
 })
 
-router.get('/page/:id', async(request: Request) => {
+router.get("/page/:id", async(request: Request) => {
     const notion = new Client({ auth: "" });
     const { id } = request.params;
     const pageId = id;
@@ -64,6 +78,6 @@ router.get('/page/:id', async(request: Request) => {
 });
 
 
-addEventListener('fetch', (event:FetchEvent) => {
+addEventListener("fetch", (event:FetchEvent) => {
   event.respondWith(router.handle(event.request))
 })
