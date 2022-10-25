03eb56aaf99180e4536d1eaf31b5ba686e5bb497
shaunregenbaum <shaunregenbaum@gmail.com>
2022-10-25 14:56:52 -0400

    simple login function now and ts ignore for global var

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Router, Route, Request } from "itty-router"
 import { Client } from "@notionhq/client";
 
+import login from "./routes/login"
+
 
 interface IMethods {
   get: Route
@@ -37,9 +39,7 @@ router.get("/", () => {
 })
 
 router.get("/login", () => { 
-    //@ts-ignore as vars are in wrangler env
-    // return Response.redirect(Redirect_URI, 301) // Commenting while edge updates
-    return Response.redirect("https://api.notion.com/v1/oauth/authorize?client_id=821734af-9bf0-47d8-bfbd-0c2f14efb15f&response_type=code", 301)
+    login();
  });
 
 router.get("/callback", async ({query}) => {
--- src/routes/login.ts
+++ src/routes/login.ts
@@ -3,7 +3,9 @@
 // On this route we simply redirect to the non-dev notion auth page. 
 // In the future we may want to expand on this for more functionality.
 export default async function login() {
-  const redirect_url = "https://api.notion.com/v1/oauth/authorize?client_id=821734af-9bf0-47d8-bfbd-0c2f14efb15f&response_type=code"
+  //For when we don't want to rely on worker vars:
+  //const redirect_url = "https://api.notion.com/v1/oauth/authorize?client_id=821734af-9bf0-47d8-bfbd-0c2f14efb15f&response_type=code"
 
-  return(Response.redirect(redirect_url, 301))
+  //@ts-ignore as var is in global
+  return(Response.redirect(Redirct_URI, 301))
 }
